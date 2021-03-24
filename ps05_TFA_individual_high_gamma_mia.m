f29148d523c16d35782196d502b31c53969ecbca
Shuai Wang <ws1011001@gmail.com>
2021-03-24 02:13:04 +0100

    cached.

--- ps05_TFA_individual_high_gamma_mia.m
+++ ps05_TFA_individual_high_gamma_mia.m
@@ -59,10 +59,15 @@ tfa='morlet';   % time-frequency analysis
 freq_step=10;
 freq_lobd=80;
 freq_upbd=120;
-baseline=[-0.2,-0.01];  % baseline from -200ms to -10ms
-timewindow=[-0.2,0.8];  % time-window of the epoch i.e. -200 ms to 800 ms
-nperm=10000;            % the number of randomizations in permutation tests
-fdr_duration=20;        % duration (in ms) for FDR correction in the time domain
+OPTIONS.nperm = 5000;
+OPTIONS.smooth = 9;  % this number x 3 < number of ROIs (for successful smoothing)
+OPTIONS.threshp = 0.05;
+OPTIONS.time = -0.2:0.001:0.8;  % from -200ms to 800ms
+OPTIONS.win_noedges = [-0.15 0.75];
+% baseline=[-0.2,-0.01];  % baseline from -200ms to -10ms
+% timewindow=[-0.2,0.8];  % time-window of the epoch i.e. -200 ms to 800 ms
+% nperm=10000;            % the number of randomizations in permutation tests
+% fdr_duration=20;        % duration (in ms) for FDR correction in the time domain
 conditions={'AAp','AAt','AVp','AVt','VAp','VAt','VVp','VVt'};
 ncond=length(conditions);
 %% ---------------------------
@@ -85,7 +90,7 @@ for i=1:n
   for icond=1:ncond
     cdir=fullfile(sdir,conditions{icond});
     fdat=fullfile(cdir,sprintf('%s_%s_%s_data_%d_%d_%d.mat',conditions{icond},mtg,tfa,freq_step,freq_lobd,freq_upbd));
-    data=load(fdat,'labels','zs');
+    data=load(fdat,'labels','zs','Time');
     % get bipolar labels and coordinates
     [bipo_mni, bipo_labels, bipo_flags]=mni_mono2bipolar(mono_mni_xyz, mono_mni.contacts, data.labels);
     roi.bipolar_mni=bipo_mni;
@@ -95,10 +100,42 @@ for i=1:n
     roi.AAL3=mni_find_regions(bipo_mni,'AAL3');
     % get ROI-specific signals
     roi.signals=roi_average_channels(data.zs(bipo_flags, :, :), bipo_mni, bipo_labels, roi.AAL3);
+    roi.time=data.Time;
     % save ROI data
     save(fdat,'roi','-append');
   end
 end
+%% ---------------------------
+
+%% between-condition comparisons for each ROI
+conditions_pairs={{'AAp','AAt'},{'AVp','AVt'},{'VAp','VAt'},{'VVp','VVt'}};
+for i=1:n
+  subj=subjects{i};
+  sdir=fullfile(bdir,subj);
+  fprintf('Perform permutation tests between conditions for each ROI for subject %s ......\n',mtg,tfa,subj);
+  % read ROI-based signals of different conditions
+  for icond=1:ncond
+    cdir=fullfile(sdir,conditions{icond});
+    fdat=fullfile(cdir,sprintf('%s_%s_%s_data_%d_%d_%d.mat',conditions{icond},mtg,tfa,freq_step,freq_lobd,freq_upbd)); 
+    load(fdat, 'roi');
+    signals.(conditions{icond})=roi.signals;
+  end
+  % compare conditions
+  for ipair=1:length(conditions_pairs)
+    cond_pair=conditions_pairs{ipair};
+    rdir=fullfile(sdir,sprintf('%s_ROIs_%s-%s',subj,cond_pair{1},cond_pair{2}));
+    if ~exist(rdir,'dir'); mkdir(rdir); end
+    fperm=fullfile(rdir,sprintf('ROIs_%s-%s_%s_%s_%d_%d_%d.mat',cond_pair{1},cond_pair{2},mtg,tfa,freq_step,freq_lobd,freq_upbd));
+    if ~exist(fperm,'file')
+      roi_perm=roi_stats_permutations(signals.(cond_pair{1}),signals.(cond_pair{2}),OPTIONS);
+      save(fperm,'roi_perm');
+    else
+      load(fperm,'roi_perm');
+    end
+    roi_plot_conditions(roi_perm,cond_pair,OPTIONS,rdir); 
+  end 
+end
+%% ---------------------------
 
 
 grpOPTIONS.mtg = 'bipolar';
@@ -186,7 +223,7 @@ tic ; [cfroi] = browse_csignal(froi,cOPTIONS);toc
 %% Compute statistical thrersholds (permutation based) : LONG PROCESSING....
 pOPTIONS.win_noedges = [-401,1601] ;
 pOPTIONS.threshp = 0.05;
-pOPTIONS.smoth = 20 ;
+pOPTIONS.smooth = 20 ;
 pOPTIONS.nperm =1000 ;
 tic ; [proi] = stats_permutations_rois(cfroi,pOPTIONS);toc
 
