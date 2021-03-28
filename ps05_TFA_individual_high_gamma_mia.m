6fd0c28de0633e0a0264fb8c84eada4ad1dd8340
Shuai Wang <ws1011001@gmail.com>
2021-03-28 01:57:19 +0100

    cached.

--- ps05_TFA_individual_high_gamma_mia.m
+++ ps05_TFA_individual_high_gamma_mia.m
@@ -60,7 +60,7 @@ freq_step=10;
 freq_lobd=80;
 freq_upbd=120;
 OPTIONS.nperm = 5000;
-OPTIONS.smooth = 9;  % this number x 3 < number of ROIs (for successful smoothing)
+OPTIONS.smooth = 5;  % this number x 3 < number of ROIs (for successful smoothing)
 OPTIONS.threshp = 0.05;
 OPTIONS.time = -0.2:0.001:0.8;  % from -200ms to 800ms
 OPTIONS.win_noedges = [-0.15 0.75];
@@ -70,70 +70,77 @@ OPTIONS.win_noedges = [-0.15 0.75];
 % fdr_duration=20;        % duration (in ms) for FDR correction in the time domain
 conditions={'AAp','AAt','AVp','AVt','VAp','VAt','VVp','VVt'};
 ncond=length(conditions);
+% set switches
+isGetROIs=true;
+isCompCon=false;
 %% ---------------------------
 
 %% extract signals for each ROI
-for i=1:n
-  subj=subjects{i};
-  sdir=fullfile(bdir,subj);
-  fprintf('Extract %s %s signals for each ROI for subject %s ......\n',mtg,tfa,subj);
-  % read the table of MNI coordinates of monopolar channels
-  fmni=fullfile(mdir,'SEEG_LectureVWFA',subj,'anat',sprintf('%s_space-MNI_contacts-monopolar.csv',subj));
-  if ~exist(fmni,'file')
-    fcoi=fullfile(mdir,'SEEG_LectureVWFA',subj,'anat/elec2atlas.mat');
-    mono_mni=mni_coi2csv(fcoi,fmni);
-  else
-    mono_mni=readtable(fmni);
-  end  
-  mono_mni_xyz=[mono_mni.x mono_mni.y mono_mni.z];
-  % calculate the signals for each ROI
-  for icond=1:ncond
-    cdir=fullfile(sdir,conditions{icond});
-    fdat=fullfile(cdir,sprintf('%s_%s_%s_data_%d_%d_%d.mat',conditions{icond},mtg,tfa,freq_step,freq_lobd,freq_upbd));
-    data=load(fdat,'labels','zs','Time');
-    % get bipolar labels and coordinates
-    [bipo_mni, bipo_labels, bipo_flags]=mni_mono2bipolar(mono_mni_xyz, mono_mni.contacts, data.labels);
-    roi.bipolar_mni=bipo_mni;
-    roi.bipolar_contacts=bipo_labels;
-    roi.bipolar_flags=bipo_flags;
-    % get regions of interest
-    roi.AAL3=mni_find_regions(bipo_mni,'AAL3');
-    % get ROI-specific signals
-    roi.signals=roi_average_channels(data.zs(bipo_flags, :, :), bipo_mni, bipo_labels, roi.AAL3);
-    roi.time=data.Time;
-    % save ROI data
-    save(fdat,'roi','-append');
+if isGetROIs
+  for i=1:n
+    subj=subjects{i};
+    sdir=fullfile(bdir,subj);
+    fprintf('Extract %s %s signals for each ROI for subject %s ......\n',mtg,tfa,subj);
+    % read the table of MNI coordinates of monopolar channels
+    fmni=fullfile(mdir,'SEEG_LectureVWFA',subj,'anat',sprintf('%s_space-MNI_contacts-monopolar.csv',subj));
+    if ~exist(fmni,'file')
+      fcoi=fullfile(mdir,'SEEG_LectureVWFA',subj,'anat/elec2atlas.mat');
+      mono_mni=mni_coi2csv(fcoi,fmni);
+    else
+      mono_mni=readtable(fmni);
+    end  
+    mono_mni_xyz=[mono_mni.x mono_mni.y mono_mni.z];
+    % calculate the signals for each ROI
+    for icond=1:ncond
+      cdir=fullfile(sdir,conditions{icond});
+      fdat=fullfile(cdir,sprintf('%s_%s_%s_data_%d_%d_%d.mat',conditions{icond},mtg,tfa,freq_step,freq_lobd,freq_upbd));
+      data=load(fdat,'labels','zs','Time');
+      % get bipolar labels and coordinates
+      [bipo_mni, bipo_labels, bipo_flags]=mni_mono2bipolar(mono_mni_xyz, mono_mni.contacts, data.labels);
+      roi.bipolar_mni=bipo_mni;
+      roi.bipolar_contacts=bipo_labels;
+      roi.bipolar_flags=bipo_flags;
+      % get regions of interest
+      roi.AAL3=mni_find_regions(bipo_mni,'AAL3');
+      % get ROI-specific signals
+      roi.signals=roi_average_channels(data.zs(bipo_flags, :, :), bipo_mni, bipo_labels, roi.AAL3);
+      roi.time=data.Time;
+      % save ROI data
+      save(fdat,'roi','-append');
+    end
   end
 end
 %% ---------------------------
 
 %% between-condition comparisons for each ROI
-conditions_pairs={{'AAp','AAt'},{'AVp','AVt'},{'VAp','VAt'},{'VVp','VVt'}};
-for i=1:n
-  subj=subjects{i};
-  sdir=fullfile(bdir,subj);
-  fprintf('Perform permutation tests between conditions for each ROI for subject %s ......\n',mtg,tfa,subj);
-  % read ROI-based signals of different conditions
-  for icond=1:ncond
-    cdir=fullfile(sdir,conditions{icond});
-    fdat=fullfile(cdir,sprintf('%s_%s_%s_data_%d_%d_%d.mat',conditions{icond},mtg,tfa,freq_step,freq_lobd,freq_upbd)); 
-    load(fdat, 'roi');
-    signals.(conditions{icond})=roi.signals;
-  end
-  % compare conditions
-  for ipair=1:length(conditions_pairs)
-    cond_pair=conditions_pairs{ipair};
-    rdir=fullfile(sdir,sprintf('%s_ROIs_%s-%s',subj,cond_pair{1},cond_pair{2}));
-    if ~exist(rdir,'dir'); mkdir(rdir); end
-    fperm=fullfile(rdir,sprintf('ROIs_%s-%s_%s_%s_%d_%d_%d.mat',cond_pair{1},cond_pair{2},mtg,tfa,freq_step,freq_lobd,freq_upbd));
-    if ~exist(fperm,'file')
-      roi_perm=roi_stats_permutations(signals.(cond_pair{1}),signals.(cond_pair{2}),OPTIONS);
-      save(fperm,'roi_perm');
-    else
-      load(fperm,'roi_perm');
+if isCompCon
+  conditions_pairs={{'AAp','AAt'},{'AVp','AVt'},{'VAp','VAt'},{'VVp','VVt'}};
+  for i=1:n
+    subj=subjects{i};
+    sdir=fullfile(bdir,subj);
+    fprintf('Perform permutation tests between conditions for each ROI for subject %s ......\n',mtg,tfa,subj);
+    % read ROI-based signals of different conditions
+    for icond=1:ncond
+      cdir=fullfile(sdir,conditions{icond});
+      fdat=fullfile(cdir,sprintf('%s_%s_%s_data_%d_%d_%d.mat',conditions{icond},mtg,tfa,freq_step,freq_lobd,freq_upbd)); 
+      load(fdat, 'roi');
+      signals.(conditions{icond})=roi.signals;
     end
-    roi_plot_conditions(roi_perm,cond_pair,OPTIONS,rdir); 
-  end 
+    % compare conditions
+    for ipair=1:length(conditions_pairs)
+      cond_pair=conditions_pairs{ipair};
+      rdir=fullfile(sdir,sprintf('%s_ROIs_%s-%s',subj,cond_pair{1},cond_pair{2}));
+      if ~exist(rdir,'dir'); mkdir(rdir); end
+      fperm=fullfile(rdir,sprintf('ROIs_%s-%s_%s_%s_%d_%d_%d.mat',cond_pair{1},cond_pair{2},mtg,tfa,freq_step,freq_lobd,freq_upbd));
+      if ~exist(fperm,'file')
+        roi_perm=roi_stats_permutations(signals.(cond_pair{1}),signals.(cond_pair{2}),OPTIONS);
+        save(fperm,'roi_perm');
+      else
+        load(fperm,'roi_perm');
+      end
+      roi_plot_conditions(roi_perm,cond_pair,OPTIONS,rdir); 
+    end 
+  end
 end
 %% ---------------------------
 
--- ps07_TFA_group_high_gamma_mia.m
+++ ps07_TFA_group_high_gamma_mia.m
@@ -61,7 +61,7 @@ freq_step=10;
 freq_lobd=80;
 freq_upbd=120;
 OPTIONS.nperm = 5000;
-OPTIONS.smooth = 9;  % this number x 3 < number of ROIs (for successful smoothing)
+OPTIONS.smooth = 5;  % this number x 3 < number of ROIs (for successful smoothing)
 OPTIONS.threshp = 0.05;
 OPTIONS.time = -0.2:0.001:0.8;  % from -200ms to 800ms
 OPTIONS.win_noedges = [-0.15 0.75];
@@ -75,7 +75,7 @@ ncond=length(conditions);
 
 %% group the contacts by ROIs
 % initialize data tables
-mni_varnames={'subjects','conditions','AAL3','mni_x','mni_y','mni_z','contacts_1','contacts_2'};
+mni_varnames={'subjects','conditions','AAL3','mni_x','mni_y','mni_z','contacts_2','contacts_1'};
 mni_table=array2table(zeros(0,length(mni_varnames)),'VariableNames',mni_varnames);
 % extract ROI info
 for icond=1:ncond
@@ -96,12 +96,120 @@ for icond=1:ncond
   end
 end
 % save group data
-fgrp=fullfile(gdir,sprintf('group_%s_%s_data-ROI_%d_%d_%d.mat',mtg,tfa,freq_step,freq_lobd,freq_upbd));
-save(fgrp,'datatable');  % it's a very big file. Don't use it if have many patients
+fmni=fullfile(gdir,sprintf('group_space-MNI_contacts-%s_ROI-AAL3',mtg));
+writetable(mni_table,[fmni '.csv']);
+save([fmni '.mat'],'mni_table');
 %% ---------------------------
 
-%% between-condition comparisons for each ROI
+%% group signals by ROIs
+rois_full=unique(mni_table.AAL3);  % get the full list of ROIs of all patients
+nrois=length(rois_full);
+rois_info=cell(nrois,5);
+for iroi=1:nrois
+  roi_name=rois_full{iroi};
+  zscores_avg=cell(n,ncond);
+  zscores_avg_avg=cell(n,ncond);
+  zscores_contacts_avg=cell(n,ncond);
+  tvals_contacts=cell(n,ncond);
+  pvals_contacts=cell(n,ncond);
+  threshdur_contacts=zeros(n,ncond);
+  contacts_labels=cell(n,ncond);
+  intersubj_corr=zeros(1,ncond);
+  interchan_corr=zeros(1,ncond);
+  for icond=1:ncond
+    con=conditions{icond};
+    for i=1:n
+      subj=subjects{i};
+      sdir=fullfile(bdir,subj);
+      fprintf('Combine ROI-based signals of condition %s for subject %s ......\n',con,subj);   
+      % load up data
+      cdir=fullfile(sdir,con);
+      fdat=fullfile(cdir,sprintf('%s_%s_%s_data_%d_%d_%d.mat',con,mtg,tfa,freq_step,freq_lobd,freq_upbd));
+      fsta=strrep(fdat,'_data','_stats');
+      load(fdat);
+      % extract ROI signals
+      if isfield(roi.signals,roi_name)
+        % within-subject between-contacts averaged z-scores (for permutation tests)
+        zscores_avg{i,icond}=roi.signals.(roi_name).avg;
+        % within-subject between-contacts across-trials averaged z-scores (for calculating ISC)
+        zscores_avg_avg{i,icond}=mean(roi.signals.(roi_name).avg,2);
+        % contact-level across-trials averaged z-scores
+        zscores_contacts_avg{i,icond}=mean(roi.signals.(roi_name).F,3);
+        % contact-level T values and P values (for showing significant contacts for each ROI)
+        load(fsta);
+        contacts_def_idx=roi_match_bipolar_labels(labels',roi.signals.(roi_name).channels);
+        tvals_contacts{i,icond}=tvals(contacts_def_idx,:);
+        pvals_contacts{i,icond}=pvals(contacts_def_idx,:);
+        threshdur_contacts(i,icond)=stats.threshdur;
+        % subject-contact labels
+        contacts_labels{i,icond}=[repmat({subj},roi.signals.(roi_name).dims(1),1) roi.signals.(roi_name).channels];
+      end
+    end
+    % number of subjects
+    subjs_idx=any(~cellfun(@isempty,contacts_labels),2);   
+    nsubjs=sum(subjs_idx);
+    % calculate inter-subject correlations
+    if nsubjs >= 2
+      isc_zscores=zscores_avg_avg(:,icond);
+      isc_zscores=cat(2,isc_zscores{:});
+      intersubj_corr(icond)=icc_kendall_w(isc_zscores,0);
+    end
+    % calculate inter-channel (across-subjects) correlations
+    icc_zscores=zscores_contacts_avg(:,icond);
+    icc_zscores=cat(1,icc_zscores{:});
+    interchan_corr(icond)=icc_kendall_w(icc_zscores',0);
+  end
+  % save the information of ROIs
+  rois_info{iroi,1}=roi_name;
+  rois_info{iroi,2}=nsubjs;
+  rois_info{iroi,3}=subjs_idx;
+  rois_info{iroi,4}=mean(intersubj_corr);
+  rois_info{iroi,5}=mean(interchan_corr);
+  finfo=fullfile(gdir,'group_Info_ROI-AAL3.mat');
+  save(finfo,'rois_info');
+  % save group data for each ROI
+  froi=fullfile(gdir,sprintf('group_N%d_ROI-AAL3-%s_%s_%s_data_%d_%d_%d.mat',nsubjs,roi_name,mtg,tfa,freq_step,freq_lobd,freq_upbd));
+  save(froi,'roi_name','subjects','conditions','nsubjs','contacts_labels','intersubj_corr',...
+            'zscores_avg','zscores_avg_avg','zscores_contacts_avg','interchan_corr',...
+            'tvals_contacts','pvals_contacts','threshdur_contacts');
+end
+%% ---------------------------
+
+%% between-condition comparisons for each ROI at the group level
 conditions_pairs={{'AAp','AAt'},{'AVp','AVt'},{'VAp','VAt'},{'VVp','VVt'}};
+for iroi=1:nrois
+  roi_name=rois_info{iroi,1};
+  roi_nsub=rois_info{iroi,2};
+  if roi_nsub >= 2 
+    % load up ROI data
+    froi=fullfile(gdir,sprintf('group_N%d_ROI-AAL3-%s_%s_%s_data_%d_%d_%d.mat',roi_nsub,roi_name,mtg,tfa,freq_step,freq_lobd,freq_upbd));
+    load(froi);
+    % read ROI-based signals of different conditions
+    for icond=1:ncond
+      zs_avg=zscores_avg(:,icond);
+      zs_avg=cat(2,zs_avg{:});
+      signals.(conditions{icond}).(roi_name).avg=zs_avg;
+    end
+    % compare conditions
+    for ipair=1:length(conditions_pairs)
+      cond_pair=conditions_pairs{ipair};
+      rdir=fullfile(gdir,sprintf('group_ROIs_%s-%s',cond_pair{1},cond_pair{2}));
+      if ~exist(rdir,'dir'); mkdir(rdir); end
+      fperm=fullfile(rdir,sprintf('ROIs_%s-%s_%s_%s_%d_%d_%d.mat',cond_pair{1},cond_pair{2},mtg,tfa,freq_step,freq_lobd,freq_upbd));
+      if ~exist(fperm,'file')
+        roi_perm=roi_stats_permutations(signals.(cond_pair{1}),signals.(cond_pair{2}),OPTIONS);
+        save(fperm,'roi_perm');
+      else
+        load(fperm,'roi_perm');
+      end
+      roi_plot_conditions(roi_perm,cond_pair,OPTIONS,rdir); 
+    end     
+  end
+end
+
+
+
+
 for i=1:n
   subj=subjects{i};
   sdir=fullfile(bdir,subj);
