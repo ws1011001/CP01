b767bbd54f312c64f7cc7b870d3289c27ea7c1d9
Shuai Wang <ws1011001@gmail.com>
2021-04-01 23:08:12 +0200

    cached.

--- ps09_CONN_individual_dynamic_connectivity.m
+++ ps09_CONN_individual_dynamic_connectivity.m
@@ -42,8 +42,8 @@ nmode=length(data_mode);                % number of data modalities
 max_r=15;                               % max radius to combine contacts
 min_w=0.3;                              % min ICC to combine contacts
 % set switches
-isGetROIs=true;
-isDynConn=false;
+isGetROIs=false;
+isDynConn=true;
 %% ---------------------------
 
 %% extract signals of ROIs
@@ -78,12 +78,15 @@ if isGetROIs
             fdat=fullfile(cdir,sprintf('%s_%s_LFP_data.mat',ctoken,mtg));
             data=load(fdat,'labels','zs','Time'); data.F=data.zs;
             roi_all_in_one(mono_mni, data, data_bl, fdat, max_r, min_w);
+            working_files.(dmode).(con)=fdat;
           case 'HGA'
             for ifreqb=1:nfreqb
               fprintf('Process %s data for condition %s ......\n',dmode,con);
               fdat=fullfile(cdir,sprintf('%s_%s_%s_data_%d_%d_%d.mat',ctoken,mtg,tfa,freqb(ifreqb,:)));
               data=load(fdat,'labels','zs','Time'); data.F=data.zs;              
               roi_all_in_one(mono_mni, data, data_bl, fdat, max_r, min_w);
+              dtoken=sprintf('%s%d',dmode,ifreqb);
+              working_files.(dtoken).(con)=fdat;
             end            
           otherwise
             error('Invalid data mode. QUIT!')
@@ -96,16 +99,44 @@ if isGetROIs
     roi_table=cell2table(roi_data,'VariableNames',{'ROI','mni_x','mni_y','mni_z','radius','longside','shortside','number_of_contacts','ICC'});
     ftab=fullfile(sdir,sprintf('%s_ROI-signals_information.csv',subj));
     writetable(roi_table,ftab);
+    % output the working files
+    fworking=fullfile(sdir,sprintf('%s_ROI-signals_working-files.mat',subj));
+    save(fworking,'working_files');
   end
 end
 %% ---------------------------
 
 %% estimate dynamic connectivity
 if isDynConn
-  % read ROIs information (manually inspected)
-  
-  % read ROIs data
-  
-  % combine clusters (as the ROI of VWFA)
+  for i=1:n
+    subj=subjects{i};
+    sdir=fullfile(bdir,subj);
+    fprintf('Estimate dynamic connectivity for subject %s ......\n',subj);
+    % read ROIs information (manually inspected)
+    ftab=fullfile(sdir,sprintf('%s_ROI-signals_information.csv',subj));
+    roi_table=readtable(ftab);
+    % select ROIs
+    roi_idx=logical(roi_table.inclusion_manual);  % index of ROIs that to be included
+    roi_networks=roi_table.subnetworks(roi_idx);  % ROI labels of sub-networks
+    roi_vwfa=ismember(roi_networks,{'VWFA'});     % ROIs to be merged as VWFA
+    % do dynamic connectivity
+    load(fullfile(sdir,sprintf('%s_ROI-signals_working-files.mat',subj)));  % read working files
+    dtokens=fieldnames(working_files);
+    for j=1:length(dtokens)
+      dtoken=dtokens{j};
+      for icond=1:ncond
+        con=conditions{icond};
+        fdat=working_files.(dtoken).(con);
+        % read ROIs data
+        load(fdat,'roi_data');
+        signals=roi_data(roi_idx,end);
+        signals=permute(cat(3, signals{:}), [3 1 2]);  % convert cell array to matrix
+        % combine clusters (as the ROI of VWFA)
+        signals_vwfa=mean(signals(roi_vwfa,:,:),1);  % average the VWFA signals - do not squeeze
+        signals(roi_vwfa,:,:)=[];                    % remove the original VWFA signals
+        signals=[signals;signals_vwfa];              % put the averaged VWFA signal at the bottom
+      end
+    end
+  end
 end
 %% ---------------------------
