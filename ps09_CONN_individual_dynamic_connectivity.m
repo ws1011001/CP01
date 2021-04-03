50f9ff060518bc066e439bf47207b764fe4d9793
Shuai Wang <ws1011001@gmail.com>
2021-04-04 01:37:18 +0200

    cached.

--- ps09_CONN_individual_dynamic_connectivity.m
+++ ps09_CONN_individual_dynamic_connectivity.m
@@ -120,7 +120,8 @@ if isDynConn
     % select ROIs
     roi_idx=logical(roi_table.inclusion_manual);  % index of ROIs that to be included
     roi_networks=roi_table.subnetworks(roi_idx);  % ROI labels of sub-networks
-    roi_vwfa=ismember(roi_networks,{'VWFA'});     % ROIs to be merged as VWFA
+    roi_language = ismember(roi_networks, {'VWFA', 'LN'});  % language network plus VWFA
+    roi_vwfa=ismember(roi_networks(roi_language), {'VWFA'});     % ROIs to be merged as VWFA
     % do dynamic connectivity
     load(fullfile(sdir,sprintf('%s_ROI-signals_working-files.mat',subj)));  % read working files
     dtokens=fieldnames(working_files);
@@ -129,24 +130,85 @@ if isDynConn
       for icond=1:ncond
         con=conditions{icond};
         fdat=working_files.(dtoken).(con);
-        % read ROIs data
-        load(fdat,'roi_data');
-        signals=roi_data(roi_idx,end);
-        signals=permute(cat(3, signals{:}), [3 1 2]);  % convert cell array to matrix
-%         % combine clusters (as the ROI of VWFA)
-%         signals_vwfa=mean(signals(roi_vwfa,:,:),1);  % average the VWFA signals - do not squeeze
-%         signals(roi_vwfa,:,:)=[];                    % remove the original VWFA signals
-%         signals=[signals;signals_vwfa];              % put the averaged VWFA signal at the bottom
-        % calculate dynamic connectivity for each trial
-        ntrials = size(signals, 3);
-        nets_dyn = cell(ntrials, 1);
-        for k = 1:ntrials
-          ts = signals(:, win_noedges(1):win_noedges(2), k);
-          nets_dyn{k} = construct_dynamic_networks(ts, win_length, overlap);
-        end        
-        % output dynamic networks
         fdyn = fullfile(sdir, sprintf('%s_dynamic_networks_%s_%s.mat', subj, dtoken, con));
-        save(fdyn, 'nets_dyn', 'win_length', 'overlap', 'fdat');
+        flan = fullfile(sdir, sprintf('%s_dynamic_networks_language_%s_%s.mat', subj, dtoken, con));
+        
+        if ~exist(fdyn, 'file')
+          fprintf('Process %s data for condition : %s ......\n', dtoken, con);
+          % read ROIs data
+          load(fdat,'roi_data');
+          signals=roi_data(roi_idx,end);
+          signals=permute(cat(3, signals{:}), [3 1 2]);  % convert cell array to matrix
+  %         % combine clusters (as the ROI of VWFA)
+  %         signals_vwfa=mean(signals(roi_vwfa,:,:),1);  % average the VWFA signals - do not squeeze
+  %         signals(roi_vwfa,:,:)=[];                    % remove the original VWFA signals
+  %         signals=[signals;signals_vwfa];              % put the averaged VWFA signal at the bottom
+          % calculate dynamic connectivity for each trial
+          ntrials = size(signals, 3);
+          nets_dyn = cell(ntrials, 1);
+          % Create progress bar during data import
+          hwait = waitbar(0,'','Units','Normalized');
+          % Make the waitbar stay on top
+          set(hwait,'WindowStyle','modal');
+          for k = 1:ntrials
+            waitbar(k/ntrials, hwait, sprintf('Process trial %d / %d', k, ntrials)) ;
+            ts = signals(:, win_noedges(1):win_noedges(2), k);
+            nets_dyn{k} = construct_dynamic_networks(ts, win_length, overlap);
+          end   
+          delete(hwait);
+          % output dynamic networks
+          save(fdyn, 'nets_dyn', 'win_noedges', 'win_length', 'overlap', 'fdat');
+        else
+          load(fdyn);
+        end
+        
+        % normalize networks by using baseline norm
+        nets_dyn = cat(4, nets_dyn{:});
+        nets_dyn_bl_mean = mean(nets_dyn(:, :, 1:10, :), 3);
+        nets_dyn_bl_std = std(nets_dyn(:, :, 1:10, :), [], 3);
+        nets_dyn_norm = (nets_dyn - repmat(nets_dyn_bl_mean, 1, 1, 39, 1)) ./ repmat(nets_dyn_bl_std, 1, 1, 39, 1);
+        nets_dyn_norm(isnan(nets_dyn_norm)) = 0;
+        % confine networks to language system        
+        nets_lang = nets_dyn_norm(roi_language, roi_language, :, :);        
+        % averaged dynamic networks
+        nets_dyn_avg = mean(nets_dyn_norm, 4);
+        nets_lang_avg = mean(nets_lang, 4);
+        % averaged strength
+        avg_strength = squeeze(mean(nets_lang(:, :, :, :), 2));
+        avg_strength_vwfa = squeeze(avg_strength(roi_vwfa, :, :));
+        
+        % basci graph measures
+        nnodes = size(nets_lang, 1);
+        nwin = size(nets_lang, 3);
+        ntrials = size(nets_lang, 4);
+        graph_measures = cell(nwin, ntrials);
+        for iwin = 1:nwin
+          tic
+          for itrial = 1:ntrials
+            net_w = nets_lang(:, :, iwin, itrial);
+            net_b = threshold_proportional(net_w, 0.5);
+            graph_measures{iwin, itrial} = metrics_basic_undirected_binary_networks(net_b);
+          end
+          toc
+        end
+        
+%         % plot averaged networks
+%         ndir = fullfile(sdir, 'Figures', sprintf('%s_dynamic_networks_language_%s_%s', subj, dtoken, con));
+%         if ~exist(ndir, 'dir'); mkdir(ndir); end         
+%         %cfg.range = [round(min(nonzeros(nets_lang_avg(:))), 2) round(max(nets_lang_avg(:)), 2)];
+%         cfg.range = [-0.5 0.5];
+%         cfg.colormap = parula(100);
+%         cfg.xticks = string(repmat({' '}, 1, size(nets_lang_avg, 1)));
+%         cfg.xticks(roi_vwfa) = 'vOT';
+%         cfg.yticks = cfg.xticks;
+%         for iwin = 1:nwin
+%           fnet = fullfile(ndir, sprintf('Network%02d.png', iwin));
+%           net_w = nets_lang_avg(:, :, iwin);
+%           plot_heatmap(net_w, fnet, cfg);
+%         end
+
+        % output the results
+        save(flan, 'nets_*', 'avg_strength', 'avg_strength_vwfa', 'graph_measures');
       end
     end
   end
