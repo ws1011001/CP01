52cecb5cb4fafb83ef898dd0e1135c5e61477f26
Shuai Wang <ws1011001@gmail.com>
2022-04-20 14:47:08 +0200

    Minor modifications.

--- ps05_TFA_individual_high_gamma_mia.m
+++ ps05_TFA_individual_high_gamma_mia.m
@@ -29,7 +29,7 @@ wdir = fullfile(mdir, 'SEEG_LectureVWFA');  % the main working folder
 rdir = fullfile(wdir, 'derivatives');       % path to the BIDS derivatives
 bdir = fullfile(rdir, protocol);            % path to the MIA database
 % read the subjects list
-fid = fopen(fullfile(mdir,'CP01_subjects.txt'));
+fid = fopen(fullfile(mdir,'CP01_subjects_lvOT.txt'));
 subjects = textscan(fid,'%s');
 fclose(fid);
 subjects = subjects{1};  % the subjects list
@@ -41,12 +41,14 @@ freq_mdbd = 80;   % the gamma frequency to separate low and high gamma
 freq_upbd = 150;  % upper boundary
 freq_bands = [freq_lobd, freq_mdbd; freq_mdbd, freq_upbd; freq_lobd, freq_upbd];
 nbands = size(freq_bands, 1);  % number of frequency bands
-
-% OPTIONS.nperm  = 5000;
-% OPTIONS.smooth = 20;  % this number x 3 < number of ROIs (for successful smoothing)
-% OPTIONS.threshp = 0.05;
-% OPTIONS.win_noedges = [-0.2 0.6];
-% OPTIONS.ylab1 = 'Gamma Activity (Z-score)';
+% statistic parameters
+stats.nboot = 5000;
+stats.alpha = 0.05;
+stats.nperm = 1000;
+stats.smooth = 20;  % this number x 3 < number of ROIs (for successful smoothing)
+stats.threshp = 0.05;
+% OPTIONS.win_noedges = [-0.2 0.6];  % plot
+% OPTIONS.ylab1 = 'Gamma Activity (Z-score)';  % plot
 
 % baseline = [-0.2,-0.01];  % baseline from -200ms to -10ms
 % timewindow = [-0.2,0.8];  % time-window of the epoch i.e. -200 ms to 800 ms
@@ -56,10 +58,11 @@ conditions = {'AAp', 'AAt', 'AVp', 'AVt', 'VAp', 'VAt', 'VVp', 'VVt', ...
               'Ap', 'Vp', 'Ap_long', 'Vp_long', 'WA', 'WV', 'PA', 'PV', 'WPA', 'WPV', ...
               'SA', 'SV', 'FA', 'FV'};
 ncond = length(conditions);
-conditions_pairs = {{'AAp', 'AAt'}, {'AVp', 'AVt'}, {'VAp', 'VAt'}, {'VVp', 'VVt'}, {'AAp', 'AVp'}, {'VVp', 'VAp'}, {'AAt', 'VAt'}, {'VVt', 'AVt'}};
+pairs = {{'AAp', 'AAt'}, {'AVp', 'AVt'}, {'VAp', 'VAt'}, {'VVp', 'VVt'}, ...  % repetition 
+         {'AAp', 'AVp'}, {'VVp', 'VAp'}, {'AAt', 'VAt'}, {'VVt', 'AVt'}, ...  % between-prime, between-target
+         {'FA', 'FV'}};  % lip-movements with audio vs. without audio 
 % set switches
-isCompChn = false;
-isPlotChn = false;
+isPlotChn = true;
 isGetROIs = false;
 isCompCon = false;
 %% ---------------------------
@@ -74,11 +77,14 @@ end
 ftmp = fullfile(bdir, 'ps05_TFA_working.mat');
 % take a note
 pdate = datetime('now','Format', 'yyyy-MM-dd''T''HH:mm:SS');  % the date of the present processing
-pnote = 'Run the pre-processing pipeline using MIA for 10 subjects (from sub-01 to sub-10).';
+%pnote = 'Run the pre-processing pipeline using MIA for 10 subjects (from sub-01 to sub-10).';
+%pnote = 'Do between-condition comparisons for 10 subjects (from sub-01 to sub-10).';
+pnote = 'Run pre-processing and statistical analysis for sub-03, sub-09 and sub-10 who have channels within the left-vOT.';
 fprintf('%s \nStart at %s. \n\n', pnote, pdate);
 if exist(ftmp, 'file')
   load(ftmp);  % read up the working file
   commits(size(commits, 1) + 1, :) = {pnote, sprintf('%s', pdate)};  % add commit
+  save(ftmp, 'commits', '-append');
 else
   commits = {pnote, sprintf('%s', pdate)};  % add commit
   save(ftmp, '*dir', 'subjects', 'n', 'freq*', 'nbands', 'conditions', 'ncond', 'commits')
@@ -142,106 +148,85 @@ if ~isfield(miacfg, 'oscillation')
 end
 %% ---------------------------
 
-%% do comparisons for each channel
-if isCompChn
-  for i =1 :n
+%% single condition statistics
+if ~isfield(miacfg, 'onesample')
+  for i = 1:n
     subj = subjects{i};
-    sdir = fullfile(bdir, subj);
-    fprintf('Perform permutation tests between conditions for each channel for subject %s. \n', subj); 
-    % do comparisons for each frequency band
+    sdir = fullfile(bdir, subj);  
     for iband = 1:nbands
-      ptoken = sprintf('%s_%s_data_%d_%d_%d', mtg, tfa, freq_step, freq_bands(iband, 1), freq_bands(iband, 2));
-      % extract signals for each channel per condition
+      freql = freq_bands(iband, 1);  % lower band
+      frequ = freq_bands(iband, 2);  % upper band           
       for icond = 1:ncond
         cdir = fullfile(sdir, conditions{icond});
-        fdat = fullfile(cdir, sprintf('%s_%s.mat', conditions{icond}, ptoken));
-        vdat = who('-file', fdat);
-        if ismember('chn', vdat)
-          load(fdat, 'chn');
-        else
-          chn = chn_signals(fdat);  % channel-wise data structure
-          save(fdat, 'chn', '-append');
-        end
-        % combine signals
-        signals.(conditions{icond}) = chn.signals;
+        fprintf('Estimate one-sample significance for frequency band %d to %d Hz for condition %s for subject %s. \n', freql, frequ, conditions{icond}, subj);
+        % statistics on monopolar signals
+        ftfa = fullfile(cdir, sprintf('%s_monopolar_morlet_data_%d_%d_%d_removeEvoked.mat', conditions{icond}, freq_step, freql, frequ));
+        mia_s5_compute_stats(ftfa, stats);  % edited mia_s5_compute_stats
+        % statistics on bipolar signals
+        ftfa = fullfile(cdir, sprintf('%s_bipolar_morlet_data_%d_%d_%d_removeEvoked.mat', conditions{icond}, freq_step, freql, frequ));
+        mia_s5_compute_stats(ftfa, stats);  % edited mia_s5_compute_stats     
       end
-      % compare conditions
-      OPTIONS.labels      = chn.labels(1, :);
-      OPTIONS.time        = chn.time;
-      OPTIONS.plot_signif = true;
-      for ipair = 1:length(conditions_pairs)
-        cond_pair = conditions_pairs{ipair};
-        rdir = fullfile(sdir, sprintf('%s-%s', cond_pair{1}, cond_pair{2}));
-        if ~exist(rdir, 'dir'); mkdir(rdir); end
-        % prepare data for one-sample permutation test (in MIA)
-        fpair = fullfile(rdir, sprintf('%s-%s_%s.mat', cond_pair{1}, cond_pair{2}, ptoken));
-        if ~exist(fpair, 'file')        
-          fprintf('Prepare data %s for one-sample permutation test of %s and %s in MIA. \n', ptoken, cond_pair{1}, cond_pair{2});
-          data.(cond_pair{1}) = load(fullfile(sdir, cond_pair{1}, sprintf('%s_%s.mat', cond_pair{1}, ptoken)));
-          data.(cond_pair{2}) = load(fullfile(sdir, cond_pair{2}, sprintf('%s_%s.mat', cond_pair{2}, ptoken)));
-          zbaseline = data.(cond_pair{1}).zbaseline;
-          freqb     = data.(cond_pair{1}).freqb;
-          Time      = data.(cond_pair{1}).Time;
-          labels    = data.(cond_pair{1}).labels;
-          history   = data.(cond_pair{1}).history;
-          F  = data.(cond_pair{1}).F - data.(cond_pair{2}).F;
-          zs = data.(cond_pair{1}).zs - data.(cond_pair{2}).zs;
-          save(fpair, 'zbaseline', 'freqb', 'Time', 'labels', 'history', 'F', 'zs');
-          clear('zbaseline', 'freqb', 'Time', 'labels', 'history', 'F', 'zs');
-        else
-          fprintf('Paired data %s for one-sample permutation test of %s and %s in MIA already exists. \n', ptoken, cond_pair{1}, cond_pair{2});
-        end
-        % two-sample permutation test            
-        fperm = fullfile(rdir, sprintf('stats-perm2_%s-%s_%s.mat', cond_pair{1}, cond_pair{2}, ptoken));
-        fprintf('Perform two-sample permutation tests between %s and %s on data %s. \n', cond_pair{1}, cond_pair{2}, ptoken);
-        tic
-        if ~exist(fperm, 'file')
-          chn_perm = roi_stats_permutations(signals.(cond_pair{1}), signals.(cond_pair{2}), OPTIONS);
-          save(fperm, 'chn_perm');
-        else
-          chn_perm = matfile(fperm, 'writable', true);
-          chn_perm = roi_stats_permutations(signals.(cond_pair{1}), signals.(cond_pair{2}), OPTIONS);
-        end
-        toc
-        OPTIONS.outputdir = fullfile(rdir, ptoken);
-        OPTIONS.figprefix = sprintf('%s_freq-%d-%d', subj, freq_bands(iband, 1), freq_bands(iband, 2));
-        roi_plot_conditions(chn_perm, cond_pair, OPTIONS);  % plot only significant results
-      end  
-    end
+    end    
   end
+  miacfg.onesample = true;
+  % update the working file
+  save(ftmp, 'miacfg', 'stats', '-append');  
 end
 %% ---------------------------
 
-%% visualize channel-wise between-condition comparisons
-if isPlotChn
-  for i =1 :n
+%% between-condition statistics (permutation tests)
+if ~isfield(miacfg, 'permutation')
+  for i =1:n
     subj = subjects{i};
     sdir = fullfile(bdir, subj);
-    fprintf('Perform permutation tests between conditions for each channel for subject %s. \n', subj); 
-    % plot comparisons for each frequency band
-    for iband = 1:nbands
-      ptoken = sprintf('%s_%s_data_%d_%d_%d', mtg, tfa, freq_step, freq_bands(iband, 1), freq_bands(iband, 2));
-      % extarc parameters
-      fdat = fullfile(sdir, 'AAp', sprintf('AAp_%s.mat', ptoken));  
-      load(fdat, 'chn');
-      OPTIONS.labels      = chn.labels(1, :);
-      OPTIONS.time        = chn.time;
-      OPTIONS.plot_signif = true;
-      for ipair = 1:length(conditions_pairs)
-        cond_pair = conditions_pairs{ipair};
-        rdir = fullfile(sdir, sprintf('%s-%s', cond_pair{1}, cond_pair{2}));
-        if ~exist(rdir, 'dir'); mkdir(rdir); end
-        % plot two-sample permutation test            
-        fperm = fullfile(rdir, sprintf('stats-perm2_%s-%s_%s.mat', cond_pair{1}, cond_pair{2}, ptoken));
-        load(fperm);
-        OPTIONS.outputdir = fullfile(rdir, ptoken);
-        OPTIONS.figprefix = sprintf('%s_freq-%d-%d', subj, freq_bands(iband, 1), freq_bands(iband, 2));
-        roi_plot_conditions(chn_perm, cond_pair, OPTIONS);  % plot only significant results
-      end  
+    fprintf('Perform permutation tests between conditions for each channel for subject %s. \n', subj);   
+    for mtg = ["monopolar", "bipolar"]  % for each montage
+      % do comparisons for each frequency band
+      for iband = 1:nbands
+        freql = freq_bands(iband, 1);  % lower band
+        frequ = freq_bands(iband, 2);  % upper band
+        ptoken = sprintf('%s_morlet_data_%d_%d_%d_removeEvoked', mtg, freq_step, freql, frequ);
+        % extract signals for each channel per condition
+        for icond = 1:ncond
+          cdir = fullfile(sdir, conditions{icond});
+          ftfa = fullfile(cdir, sprintf('%s_%s.mat', conditions{icond}, ptoken));
+          vtfa = who('-file', ftfa);
+          if ismember('zs_chn', vtfa)
+            load(ftfa, 'zs_chn');
+          else
+            zs_chn = chn_signals(ftfa);  % channel-wise data structure
+            save(ftfa, 'zs_chn', '-append');
+          end
+          % group conditions
+          signals.(conditions{icond}) = zs_chn.signals;
+        end
+        % compare conditions
+        cfg = stats;
+        cfg.labels      = zs_chn.labels(1, :);
+        cfg.time        = zs_chn.time;
+        cfg.plot_signif = true;
+        for ipair = 1:length(pairs)
+          cpair = pairs{ipair};
+          cdir = fullfile(sdir, 'contrasts', sprintf('%s-%s', cpair{1}, cpair{2}));
+          if ~exist(cdir, 'dir'); mkdir(cdir); end
+          % two-sample permutation test            
+          fperm = fullfile(cdir, sprintf('%s-%s_%s.mat', cpair{1}, cpair{2}, ptoken));
+          fprintf('Perform two-sample permutation tests between %s and %s on data %s. \n', cpair{1}, cpair{2}, ptoken);
+          tic  % start permutation test
+          zs_chn_perm = roi_stats_permutations(signals.(cpair{1}), signals.(cpair{2}), cfg);
+          save(fperm, 'zs_chn_perm');
+          toc  % done this test. e.g., 277s for FA-FV 1st band sub-10
+        end  
+      end
     end
-  end  
+  end
+  miacfg.permutation = true;
+  % update the working file
+  save(ftmp, 'miacfg', '-append');   
 end
 %% ---------------------------
+
+
 
 %% extract signals for each ROI
 if isGetROIs
@@ -261,8 +246,8 @@ if isGetROIs
     % calculate the signals for each ROI
     for icond = 1:ncond
       cdir = fullfile(sdir, conditions{icond});
-      fdat = fullfile(cdir, sprintf('%s_%s.mat', conditions{icond}, ptoken));
-      data = load(fdat, 'labels', 'zs', 'Time');
+      ftfa = fullfile(cdir, sprintf('%s_%s.mat', conditions{icond}, ptoken));
+      data = load(ftfa, 'labels', 'zs', 'Time');
       % get bipolar labels and coordinates
       [bipo_mni, bipo_labels, bipo_flags] = mni_mono2bipolar(mono_mni_xyz, mono_mni.contacts, data.labels);
       roi.bipolar_mni = bipo_mni;
@@ -274,7 +259,7 @@ if isGetROIs
       roi.signals = roi_average_channels(data.zs(bipo_flags, :, :), bipo_mni, bipo_labels, roi.AAL3);
       roi.time = data.Time;
       % save ROI data
-      save(fdat, 'roi', '-append');
+      save(ftfa, 'roi', '-append');
     end
   end
 end
@@ -289,23 +274,23 @@ if isCompCon
     % read ROI-based signals of different conditions
     for icond = 1:ncond
       cdir = fullfile(sdir, conditions{icond});
-      fdat = fullfile(cdir, sprintf('%s_%s.mat', conditions{icond}, ptoken)); 
-      load(fdat, 'roi');
+      ftfa = fullfile(cdir, sprintf('%s_%s.mat', conditions{icond}, ptoken)); 
+      load(ftfa, 'roi');
       signals.(conditions{icond}) = roi.signals;
     end
     % compare conditions
-    for ipair = 1:length(conditions_pairs)
-      cond_pair = conditions_pairs{ipair};
-      rdir = fullfile(sdir, sprintf('%s_ROIs_%s-%s', subj, cond_pair{1}, cond_pair{2}));
+    for ipair = 1:length(pairs)
+      cpair = pairs{ipair};
+      rdir = fullfile(sdir, sprintf('%s_ROIs_%s-%s', subj, cpair{1}, cpair{2}));
       if ~exist(rdir, 'dir'); mkdir(rdir); end
-      fperm = fullfile(rdir, sprintf('ROIs_%s-%s_%s.mat', cond_pair{1}, cond_pair{2}, ptoken));
+      fperm = fullfile(rdir, sprintf('ROIs_%s-%s_%s.mat', cpair{1}, cpair{2}, ptoken));
       if ~exist(fperm, 'file')
-        roi_perm = roi_stats_permutations(signals.(cond_pair{1}), signals.(cond_pair{2}), OPTIONS);
+        roi_perm = roi_stats_permutations(signals.(cpair{1}), signals.(cpair{2}), OPTIONS);
         save(fperm, 'roi_perm');
       else
         load(fperm, 'roi_perm');
       end
-      roi_plot_conditions(roi_perm, cond_pair, OPTIONS, rdir); 
+      roi_plot_conditions(roi_perm, cpair, OPTIONS, rdir); 
     end 
   end
 end
